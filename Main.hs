0feb1887fdceb6dc928a0993265b1b4202dc26f3
John Children <john.a.children@gmail.com>
2019-02-17 00:01:35 +0000

    Flesh out some docstests

--- Main.hs
+++ Main.hs
@@ -22,11 +22,14 @@ mkAtom s = Data (AtomData (AtomID s))
 
 type Parser a = Parsec String () a
 
--- |
+-- | Pie type parser
+--
+-- Examples:
+--
 -- >>> parse pieType "" "Atom"
 -- Right AtomType
 pieType :: Parser PieType
-pieType = (AtomType <$ string "Atom") <|> undefined
+pieType = (AtomType <$ string "Atom")
 
 -- Atom IDs must only contain letters and hyphens
 atomID :: Parser AtomID
@@ -35,21 +38,31 @@ atomID = do
     id <- many (alphaNum <|> char '-')
     pure $ AtomID id
 
--- |
+-- | Pie data parser
 -- >>> parse pieData "" "'courgette"
 -- Right (AtomData (AtomID "courgette"))
 pieData :: Parser PieData
-pieData = (AtomData <$> atomID) <|> undefined
+pieData = (AtomData <$> atomID)
 
 pie :: Parser Pie
 pie = (Type <$> pieType) <|> (Data <$> pieData)
+
+parsePie :: String -> Either ParseError Pie
+parsePie = parse pie "<lit>"
+
+parsePieOrThrow :: String -> Pie
+parsePieOrThrow s = case parsePie s of
+    Right pie -> pie
+    Left err -> error (show err)
 
 -- | First form of judgement
 -- ______ is a ______.
 --
 -- Examples:
 --
--- >>> judgement1 (mkAtom "courgette") (Type AtomType)
+-- >>> let courgette = parsePieOrThrow "'courgette"
+-- >>> let atomType = parsePieOrThrow "Atom"
+-- >>> judgement1 courgette atomType
 -- True
 judgement1 :: Pie -> Pie -> Bool
 judgement1 (Data d) (Type t) = judgement1' d t
@@ -66,10 +79,15 @@ judgement1' _ _ = False
 --
 -- Examples:
 --
--- >>> judgement2 (mkAtom "ratatouille") (Type AtomType) (mkAtom "ratatouille")
+-- >>> let courgette = parsePieOrThrow "'courgette"
+-- >>> let atomType = parsePieOrThrow "Atom"
+-- >>> judgement2 courgette atomType courgette
 -- True
 --
--- >>> judgement2 (mkAtom "ratatouille") (Type AtomType) (mkAtom "baguette")
+-- >>> let courgette = parsePieOrThrow "'courgette"
+-- >>> let atomType = parsePieOrThrow "Atom"
+-- >>> let baguette = parsePieOrThrow "'baguette"
+-- >>> judgement2 courgette atomType baguette
 -- False
 judgement2 :: Pie -> Pie -> Pie -> Bool
 judgement2 (Data d1) (Type t) (Data d2) = judgement2' d1 t d2
@@ -78,17 +96,37 @@ judgement2 _ _ _ = False
 -- After pre-supposition
 judgement2' :: PieData -> PieType -> PieData -> Bool
 judgement2' (AtomData id1) AtomType (AtomData id2) = id1 == id2
-judgement2' cd1@(ConsData c1 c2) pt@(PairType p1 p2) cd2@(ConsData c3 c4) = judgement2' c1 p1 c3 && judgement2' c2 p2 c4
+judgement2' (ConsData c1 c2) (PairType p1 p2) (ConsData c3 c4) = judgement2' c1 p1 c3 && judgement2' c2 p2 c4
 judgement2' _ _ _ = False
 
--- third form of judgement
+-- | Third form of judgement
 -- _____ is a type.
+--
+-- Examples:
+--
+-- >>> let courgette = parsePieOrThrow "'courgette"
+-- >>> judgement3 courgette
+-- False
+--
+-- >>> let atomType = parsePieOrThrow "Atom"
+-- >>> judgement3 atomType
+-- True
 judgement3 :: Pie -> Bool
 judgement3 (Type _) = True
 judgement3 (Data _) = False
 
 -- fourth form of judgement
 -- ______ and ______ are the same type.
+--
+-- Examples:
+--
+-- >>> let courgette = parsePieOrThrow "'courgette"
+-- >>> judgement4 courgette courgette
+-- False
+--
+-- >>> let atomType = parsePieOrThrow "Atom"
+-- >>> judgement4 atomType atomType
+-- True
 judgement4 :: Pie -> Pie -> Bool
 judgement4 (Type t1) (Type t2) = judgement4' t1 t2
 judgement4 _ _ = False
