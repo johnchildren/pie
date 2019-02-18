0a4ff1696e542fdf628d543afcb4b7b7b6bcb649
John Children <john.a.children@gmail.com>
2019-02-18 22:43:34 +0000

    Make expr a functor
    Also adds some recursion schemes

--- Main.hs
+++ Main.hs
@@ -1,17 +1,29 @@
+{-# LANGUAGE DeriveFunctor #-}
+
 module Main where
 
+import           Control.Category               ( (>>>) )
 import           Text.Parsec
 
 newtype AtomID = AtomID String
     deriving (Show, Eq)
 
-data Pie = Pair Pie Pie
-         | Cons Pie Pie
+newtype Term f = In { out :: f (Term f) }
+
+type Algebra f a = f a -> a
+
+cata :: (Functor f) => Algebra f a -> Term f -> a
+cata f = out >>> fmap (cata f) >>> f
+
+data Expr f = Pair f f
+         | Cons f f
          | AtomType
          | AtomData AtomID
-         | Car Pie
-         | Cdr Pie
-         deriving (Show)
+         | Car f
+         | Cdr f
+         | Zero
+         | Add1 f
+         deriving (Show, Functor)
 
 type Parser a = Parsec String () a
 
@@ -28,12 +40,11 @@ atomID = do
   id <- many (alphaNum <|> char '-')
   pure $ AtomID id
 
-printUnaryExpr :: String -> Pie -> String
-printUnaryExpr tok e1 = "(" ++ tok ++ " " ++ printPie e1 ++ ")"
+printUnaryExpr :: String -> String -> String
+printUnaryExpr tok e1 = "(" ++ tok ++ " " ++ e1 ++ ")"
 
-printBinaryExpr :: String -> Pie -> Pie -> String
-printBinaryExpr tok e1 e2 =
-  "(" ++ tok ++ " " ++ printPie e1 ++ " " ++ printPie e2 ++ ")"
+printBinaryExpr :: String -> String -> String -> String
+printBinaryExpr tok e1 e2 = "(" ++ tok ++ " " ++ e1 ++ " " ++ e2 ++ ")"
 
 -- | Print an expression
 --
@@ -46,29 +57,40 @@ printBinaryExpr tok e1 e2 =
 -- >>> let pairType = parsePieOrThrow "(Pair Atom Atom)"
 -- >>> printPie pairType
 -- "(Pair Atom Atom)"
-printPie :: Pie -> String
-printPie AtomType              = "Atom"
-printPie (AtomData (AtomID s)) = "'" ++ s
-printPie (Cons e1 e2         ) = printBinaryExpr "cons" e1 e2
-printPie (Pair e1 e2         ) = printBinaryExpr "Pair" e1 e2
-printPie (Car e1             ) = printUnaryExpr "car" e1
-printPie (Cdr e1             ) = printUnaryExpr "cdr" e1
+--
+-- >>> let four = parsePieOrThrow "(add1 (add1 (add1 (add1 zero))))"
+-- >>> printPie four
+-- "(add1 (add1 (add1 (add1 zero))))"
+printPie :: Term Expr -> String
+printPie = cata printPie'
 
-parseUnaryExpr :: Parser (Pie -> Pie) -> Parser Pie
+printPie' :: Expr String -> String
+printPie' (AtomType             ) = "Atom"
+printPie' ((AtomData (AtomID s))) = "'" ++ s
+printPie' (Zero                 ) = "zero"
+printPie' ((Cons e1 e2)         ) = printBinaryExpr "cons" e1 e2
+printPie' ((Pair e1 e2)         ) = printBinaryExpr "Pair" e1 e2
+printPie' ((Car  e1   )         ) = printUnaryExpr "car" e1
+printPie' ((Cdr  e1   )         ) = printUnaryExpr "cdr" e1
+printPie' ((Add1 e1   )         ) = printUnaryExpr "add1" e1
+
+parseUnaryExpr :: Parser (Term Expr -> Term Expr) -> Parser (Term Expr)
 parseUnaryExpr p = p <*> (spaces1 >> pie)
 
-parseBinaryExpr :: Parser (Pie -> Pie -> Pie) -> Parser Pie
+parseBinaryExpr
+  :: Parser (Term Expr -> Term Expr -> Term Expr) -> Parser (Term Expr)
 parseBinaryExpr p = p <*> (spaces1 >> pie) <*> (spaces1 >> pie)
 
-pieExpr :: Parser Pie
+pieExpr :: Parser (Term Expr)
 pieExpr =
-  (   parseBinaryExpr (Pair <$ string "Pair")
+  (   parseBinaryExpr ((\x y -> In (Pair x y)) <$ string "Pair")
   <|> (  string "c"
-      >> (   parseBinaryExpr (Cons <$ string "ons")
-         <|> parseUnaryExpr (Car <$ string "ar")
-         <|> parseUnaryExpr (Cdr <$ string "dr")
+      >> (   parseBinaryExpr ((\x y -> In (Cons x y)) <$ string "ons")
+         <|> parseUnaryExpr ((\x -> In (Car x)) <$ string "ar")
+         <|> parseUnaryExpr ((\x -> In (Cdr x)) <$ string "dr")
          )
       )
+  <|> parseUnaryExpr ((\x -> In (Add1 x)) <$ string "add1")
   )
 
 
@@ -76,21 +98,28 @@ pieExpr =
 --
 -- Examples:
 --
--- >>> parse pie "" "Atom"
--- Right AtomType
+-- >>> let atomType = parse pie "" "Atom"
+-- >>> printPie <$> atomType
+-- Right "Atom"
 --
--- >>> parse pie "" "(Pair Atom Atom)"
--- Right (Pair AtomType AtomType)
+-- >>> let pairType = parse pie "" "(Pair Atom Atom)"
+-- >>> printPie <$> pairType
+-- Right "(Pair Atom Atom)"
 --
--- >>> parse pie "" "'courgette"
--- Right (AtomData (AtomID "courgette"))
-pie :: Parser Pie
-pie = (AtomType <$ string "Atom") <|> (AtomData <$> atomID) <|> parens pieExpr
+-- >>> let atom = parse pie "" "'courgette"
+-- >>> printPie <$> atom
+-- Right "'courgette"
+pie :: Parser (Term Expr)
+pie =
+  ((In AtomType) <$ string "Atom")
+    <|> ((\x -> In (AtomData x)) <$> atomID)
+    <|> ((In Zero) <$ string "zero")
+    <|> parens pieExpr
 
-parsePie :: String -> Either ParseError Pie
+parsePie :: String -> Either ParseError (Term Expr)
 parsePie = parse pie "<lit>"
 
-parsePieOrThrow :: String -> Pie
+parsePieOrThrow :: String -> (Term Expr)
 parsePieOrThrow s = case parsePie s of
   Right pie -> pie
   Left  err -> error (show err)
@@ -109,10 +138,11 @@ parsePieOrThrow s = case parsePie s of
 -- >>> let pairType = parsePieOrThrow "(Pair Atom Atom)"
 -- >>> judgement1 consData pairType
 -- True
-judgement1 :: Pie -> Pie -> Bool
-judgement1 (AtomData _) AtomType     = True
-judgement1 (Cons d1 d2) (Pair t1 t2) = judgement1 d1 t1 && judgement1 d2 t2
-judgement1 _            _            = False
+judgement1 :: Term Expr -> Term Expr -> Bool
+judgement1 (In (AtomData _)) (In AtomType) = True
+judgement1 (In (Cons d1 d2)) (In (Pair t1 t2)) =
+  judgement1 d1 t1 && judgement1 d2 t2
+judgement1 _ _ = False
 
 -- | Second form of judgement
 -- ______ is the same ______ as ______.
@@ -129,9 +159,9 @@ judgement1 _            _            = False
 -- >>> let baguette = parsePieOrThrow "'baguette"
 -- >>> judgement2 courgette atomType baguette
 -- False
-judgement2 :: Pie -> Pie -> Pie -> Bool
-judgement2 (AtomData id1) AtomType (AtomData id2) = id1 == id2
-judgement2 (Cons c1 c2) (Pair p1 p2) (Cons c3 c4) =
+judgement2 :: Term Expr -> Term Expr -> Term Expr -> Bool
+judgement2 (In (AtomData id1)) (In AtomType) (In (AtomData id2)) = id1 == id2
+judgement2 (In (Cons c1 c2)) (In (Pair p1 p2)) (In (Cons c3 c4)) =
   judgement2 c1 p1 c3 && judgement2 c2 p2 c4
 judgement2 _ _ _ = False
 
@@ -147,10 +177,10 @@ judgement2 _ _ _ = False
 -- >>> let atomType = parsePieOrThrow "Atom"
 -- >>> judgement3 atomType
 -- True
-judgement3 :: Pie -> Bool
-judgement3 AtomType   = True
-judgement3 (Pair _ _) = True -- Not strictly true
-judgement3 _          = False
+judgement3 :: Term Expr -> Bool
+judgement3 (In AtomType  ) = True
+judgement3 (In (Pair _ _)) = True -- Not strictly true
+judgement3 _               = False
 
 -- fourth form of judgement
 -- ______ and ______ are the same type.
@@ -164,10 +194,11 @@ judgement3 _          = False
 -- >>> let atomType = parsePieOrThrow "Atom"
 -- >>> judgement4 atomType atomType
 -- True
-judgement4 :: Pie -> Pie -> Bool
-judgement4 AtomType     AtomType     = True
-judgement4 (Pair p1 p2) (Pair p3 p4) = judgement4 p1 p3 && judgement4 p2 p4
-judgement4 _            _            = False
+judgement4 :: Term Expr -> Term Expr -> Bool
+judgement4 (In AtomType) (In AtomType) = True
+judgement4 (In (Pair p1 p2)) (In (Pair p3 p4)) =
+  judgement4 p1 p3 && judgement4 p2 p4
+judgement4 _ _ = False
 
 data TypeError = TypeError
   deriving (Show)
@@ -179,23 +210,25 @@ data TypeError = TypeError
 -- >>> let expr = parsePieOrThrow "(car (cons (cons 'aubergine 'courgette) 'tomato))"
 -- >>> printPie <$> eval expr
 -- Right "(cons 'aubergine 'courgette)"
-eval :: Pie -> Either TypeError Pie
-eval AtomType            = Right AtomType
-eval a@(AtomData _     ) = Right a
-eval c@(Cons _ _       ) = Right c
-eval p@(Pair _ _       ) = Right p
-eval (  Car (Cons v1 _)) = Right v1
-eval (  Car (Pair v1 _)) = Right v1
-eval (  Car _          ) = Left TypeError
-eval (  Cdr (Cons _ v2)) = Right v2
-eval (  Cdr (Pair _ v2)) = Right v2
-eval (  Cdr _          ) = Left TypeError
-
-
-normalize :: Pie -> Either TypeError Pie
-normalize = undefined
+--
+-- >>> let expr = parsePieOrThrow "(Pair (car (cons Atom 'olive)) (cdr (cons 'oil Atom)))"
+-- >>> printPie <$> eval expr
+-- Right "(Pair Atom Atom)"
+eval :: Term Expr -> Either TypeError (Term Expr)
+eval (In AtomType                ) = Right (In AtomType)
+eval (In a@(AtomData _          )) = Right (In a)
+eval (In (Cons e1 e2)) = (\x y -> In (Cons x y)) <$> (eval e1) <*> (eval e2)
+eval (In (Pair e1 e2)) = (\x y -> In (Pair x y)) <$> (eval e1) <*> (eval e2)
+eval (In (  Car (In (Cons v1 _)))) = Right v1
+eval (In (  Car (In (Pair v1 _)))) = Right v1
+eval (In (  Car _               )) = Left TypeError
+eval (In (  Cdr (In (Cons _ v2)))) = Right v2
+eval (In (  Cdr (In (Pair _ v2)))) = Right v2
+eval (In (  Cdr _               )) = Left TypeError
+eval (In Zero                    ) = Right (In Zero)
+eval (In (Add1 e1)               ) = (\x -> In (Add1 x)) <$> eval e1
 
 main :: IO ()
 main = do
   let foo = parsePieOrThrow "'foo"
-  print $ judgement2 foo AtomType foo
+  print $ judgement2 foo (In AtomType) foo
