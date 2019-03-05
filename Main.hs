44228078595bf5dd17732901024bc0c92e3a5b91
John Children <john.a.children@gmail.com>
2019-03-05 14:08:50 +0000

    minor repl QOL improvements

--- Main.hs
+++ Main.hs
@@ -10,7 +10,9 @@ import           System.IO                                ( hFlush
                                                           , stdout
                                                           )
 import qualified Data.Text.IO                  as Text
-import           Language.Pie.Parse                       ( parsePie )
+import           Language.Pie.Parse                       ( parsePie
+                                                          , parseErrorPretty
+                                                          )
 import           Language.Pie.Print                       ( printPie )
 import           Language.Pie.Eval                        ( emptyEnv
                                                           , evalPie
@@ -25,13 +27,13 @@ main = do
  where
   go :: IO ()
   go = do
-    Text.putStr "pie)) "
+    Text.putStr "pie> "
     hFlush stdout
     line <- Text.getLine
     case parsePie line of
       Right expr -> case evalPie emptyEnv expr of
         Right evald -> Text.putStrLn (printPie evald)
         Left  err   -> print err
-      Left err -> print err
+      Left err -> putStrLn $ parseErrorPretty err
     putStrLn ""
     go
--- src/Language/Pie/Parse.hs
+++ src/Language/Pie/Parse.hs
@@ -2,6 +2,7 @@
 
 module Language.Pie.Parse
   ( parsePie
+  , parseErrorPretty
   )
 where
 
