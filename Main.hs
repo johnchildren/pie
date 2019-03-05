5f92ecd46915f830918160344d9d0459d1899637
John Children <john.a.children@gmail.com>
2019-03-05 09:08:32 +0000

    Implements the most basic of REPLs

--- Main.hs
+++ Main.hs
@@ -3,14 +3,32 @@ module Main
   )
 where
 
-import           Language.Pie.Eval                        ( emptyEnv )
-import           Language.Pie.Judgement                   ( judgement2 )
-import           Language.Pie.Expr                        ( AtomID(..)
-                                                          , Expr(..)
+
+import           System.IO                                ( hFlush
+                                                          , stdout
                                                           )
+import           Language.Pie.Parse                       ( parsePie )
+import           Language.Pie.Print                       ( printPie )
+import           Language.Pie.Eval                        ( emptyEnv
+                                                          , evalPie
+                                                          )
+
 
 
 main :: IO ()
 main = do
-  let foo = AtomData (AtomID "foo")
-  print $ judgement2 emptyEnv foo AtomType foo
+  putStrLn "Welcome to lazyfuck! Each line will be evaluated as a script!"
+  go
+ where
+  go :: IO ()
+  go = do
+    putStr "pie)) "
+    hFlush stdout
+    line <- getLine
+    case parsePie line of
+      Right expr -> case evalPie emptyEnv expr of
+        Right evald -> putStrLn (printPie evald)
+        Left  err   -> print err
+      Left err -> print err
+    putStrLn ""
+    go
