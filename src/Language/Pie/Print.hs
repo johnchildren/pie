9107fe2ea037f4f36ab848bc75e1771ef3b295fe
John Children <john.a.children@gmail.com>
2019-08-16 08:47:24 +0100

    Italicize dimmed variables in ANSI output
    Makes dimmed variables a bit clearer by outputting them in ANSI italics.
    Obviously not really portable but works okay for now.

--- src/Language/Pie/Print.hs
+++ src/Language/Pie/Print.hs
@@ -9,7 +9,6 @@ import           Data.Function                            ( (.)
                                                           )
 import           Data.Eq                                  ( (==) )
 import qualified Data.List.NonEmpty            as NonEmpty
-import           Data.Text.Prettyprint.Doc.Render.Text    ( renderStrict )
 import           Data.Text                                ( Text )
 import           Data.Text.Prettyprint.Doc                ( (<>)
                                                           , (<+>)
@@ -22,6 +21,12 @@ import           Data.Text.Prettyprint.Doc                ( (<>)
                                                           , space
                                                           , layoutPretty
                                                           , defaultLayoutOptions
+                                                          , annotate
+                                                          )
+import           Data.Text.Prettyprint.Doc.Render.Terminal
+                                                          ( AnsiStyle
+                                                          , italicized
+                                                          , renderStrict
                                                           )
 import           Data.Functor.Foldable                    ( Base
                                                           , cata
@@ -40,12 +45,13 @@ parens :: [Doc a] -> Doc a
 parens = encloseSep lparen rparen space
 
 -- for sigma and pi
-typePair :: (VarName, Doc a) -> Doc a
+typePair :: (VarName, Doc AnsiStyle) -> Doc AnsiStyle
 typePair (x, y) = enclose lparen rparen (printVarName x <+> y)
 
-printVarName :: VarName -> Doc a
+printVarName :: VarName -> Doc AnsiStyle
 printVarName (VarName v n) = pretty v <> if n == 0 then "" else pretty n
-printVarName (Dimmed  v _) = "_" <> pretty v <> "_"
+printVarName (Dimmed v n) =
+  annotate italicized (pretty v <> if n == 0 then "" else pretty n)
 
 printUnaryExpr :: Doc a -> Doc a -> Doc a
 printUnaryExpr tok e1 = parens [tok, e1]
@@ -61,7 +67,7 @@ type Algebra t a = Base t a -> a
 printPie :: Expr -> Text
 printPie = renderStrict . layoutPretty defaultLayoutOptions . cata printPie'
  where
-  printPie' :: Algebra Expr (Doc a)
+  printPie' :: Algebra Expr (Doc AnsiStyle)
   printPie' (TheF e1 e2)        = printBinaryExpr "the" e1 e2
   printPie' (VarF v    )        = printVarName v
   printPie' AtomF               = "Atom"
@@ -74,9 +80,11 @@ printPie = renderStrict . layoutPretty defaultLayoutOptions . cata printPie'
   printPie' (LambdaF vs e) =
     printBinaryExpr "lambda" (parens $ printVarName <$> NonEmpty.toList vs) e
   printPie' (PiF vs e2) =
-    printBinaryExpr "Pi" (parens [(parens (typePair <$> NonEmpty.toList vs))]) e2
-  printPie' (SigmaF vs e2) =
-    printBinaryExpr "Sigma" (parens [(parens (typePair <$> NonEmpty.toList vs))]) e2
+    printBinaryExpr "Pi" (parens [parens (typePair <$> NonEmpty.toList vs)]) e2
+  printPie' (SigmaF vs e2) = printBinaryExpr
+    "Sigma"
+    (parens [parens (typePair <$> NonEmpty.toList vs)])
+    e2
   printPie' (AppF e es)          = parens (e : NonEmpty.toList es)
   printPie' NatF                 = "Nat"
   printPie' ZeroF                = "zero"
